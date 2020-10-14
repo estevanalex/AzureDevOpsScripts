1aec43eb0a80897268b29c59e8bb23c4941b6fc1
estevanalex <62678236+estevanalex@users.noreply.github.com>
2020-10-14 13:45:18 +1100

    Update ThreadfixFoDSync.ps1

--- ThreadfixFoDSync.ps1
+++ ThreadfixFoDSync.ps1
@@ -25,5 +25,29 @@ $headers = New-Object "System.Collections.Generic.Dictionary[[String],[String]]"
 $headers.Add("Authorization", "APIKEY " + $TF_APIKey)
 $headers.Add("Accept", "application/json")
 
-# Sync
-$_results = Invoke-RestMethod -Uri $TF_URL -Headers $headers -Method Post 
+# Sync in four attempts
+$_attempts = 1
+$_looperror = 0
+do 
+{ 
+    $_results = try { Invoke-RestMethod -Uri $TF_URL -Headers $headers -Method Post } catch { $_.Exception.Response }
+    $_httpcode = $_results.StatusCode
+    if (!$_httpcode)
+    {
+        $_looperror = 0
+        $_attempts = 5
+    } else {
+        Write-Host "##vso[task.LogIssue type=warning;]Attempt $_attempts failed due" $_results.StatusCode
+        $_attempts++
+        $_looperror = 1
+        Start-Sleep 4
+    } 
+} while ( $_attempts -le 4 )
+
+if ( $_looperror -eq 1 )
+{
+   Write-Host "##vso[task.LogIssue type=error;]Synchronization for FoD and Threafix is not working!!! Do it manually via Threadfix Application page!!!!"
+   Write-Host "##vso[task.complete result=SucceededWithIssues;]"
+} else {
+   Write-Host "Threadfix and Fortify On Demand Synchronization for Application $TF_APPID has been done!"
+}
